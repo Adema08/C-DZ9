ca13afb900d10b967c74f74c0b899ee9165ae238
Adema <aatygaeva08@gmail.com>
2023-06-11 20:02:03 +0600

    1.от н до 1 2.сумма между м и н 3.функция аккермана

---
+++ task 66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumMtoN(int m, int n)
+{
+    if (n == m)
+    {
+        return m;
+    }
+    return m + SumMtoN(m + 1, n);
+}
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(SumMtoN(m, n));
